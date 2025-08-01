a8c7da50e0665944aaad66aa9ad33c0f6ab2b54d
anand123-kumar <anandtupakula1234@gmail.com>
2025-08-01 11:08:36 +0530

    Update GradeServlet.java

--- src/main/java/myservletprograms/GradeServlet.java
+++ src/main/java/myservletprograms/GradeServlet.java
@@ -8,15 +8,15 @@ import javax.servlet.http.*;
 @WebServlet("/grade")
 public class GradeServlet extends HttpServlet {
  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-     // Set response type
+   
      response.setContentType("text/html");
      PrintWriter out = response.getWriter();
 
-     // Retrieve forwarded data
+     
      String name = (String) request.getAttribute("name");
      int marks = Integer.parseInt((String) request.getAttribute("marks"));
 
-     // Grade logic
+     
      String grade;
      if (marks >= 90) grade = "A";
      else if (marks >= 75) grade = "B";
@@ -24,7 +24,7 @@ public class GradeServlet extends HttpServlet {
      else if (marks >= 40) grade = "D";
      else grade = "F";
 
-     // Display result
+    
      out.println("<html><body>");
      out.println("<h2>Student Result</h2>");
      out.println("<p>Name: " + name + "</p>");

3504f88410c9ea39d02ab4bdb138fc37eef3bc61
anand123-kumar <anandtupakula1234@gmail.com>
2025-08-01 11:12:51 +0530

    Update RegisterServlet.java

--- src/main/java/myservletprograms/RegisterServlet.java
+++ src/main/java/myservletprograms/RegisterServlet.java
@@ -7,17 +7,15 @@ import javax.servlet.http.*;
 @WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-     // Set response type
      response.setContentType("text/html");
      PrintWriter out = response.getWriter();
 
-     // Read form data
      String name = request.getParameter("name");
      String email = request.getParameter("email");
      String gender = request.getParameter("gender");
      String course = request.getParameter("course");
 
-     // Display the submitted data
+    
      out.println("<html><body>");
      out.println("<h2>Registration Details</h2>");
      out.println("<p>Name: " + name + "</p>");

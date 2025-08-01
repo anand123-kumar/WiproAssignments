bf6800eeb324ccf7e236c2eb60793be62669c373
anand123-kumar <anandtupakula1234@gmail.com>
2025-08-01 11:00:33 +0530

    Update EmployeeDAO.java

--- src/main/java/com/mockitoprograms/EmployeeDAO.java
+++ src/main/java/com/mockitoprograms/EmployeeDAO.java
@@ -3,45 +3,40 @@ import java.util.Collections;
 import java.util.List;
 
 public class EmployeeDAO {
-	// creating an object
+	
 	private static EmployeeDAO employeeDAO = new EmployeeDAO();
 
-	/*
-	 * private constructor. No objects of this class be created with new
-	 * EmployeeDAO(). Use getInstance() instead
-	 */
+
 	private EmployeeDAO() {
 	}
 
-	// static method to create object
+	
 	public static EmployeeDAO getInstance() {
 		return employeeDAO;//ONLY STATIC TYPE
 	}
-	// get employee
+	
 	public Employee getEmployee(Long employeeId) {
 		return null;
 	}
 
-	// list all employees
-	//Collection is a built in class!
+	
 	public List<Employee> getAll() {
 		return Collections.emptyList();
 	}
 
-	// add employee
+	
 	public String addEmployee(Employee employee) {
 		return employee.getEmployeeName();//returning empname
 	}
 	
-	// update employee
+	
 	public String updateEmployee(Employee employee) {
 		return employee.getEmployeeName();//returning empname
 	}
-	// delete employee
+	
 	public String deleteEmployee(Long employeeId) {
 		return null;
 	}
-	// get points
 	public int getPoints(Long employeeId) {
 		return 0;
 	}

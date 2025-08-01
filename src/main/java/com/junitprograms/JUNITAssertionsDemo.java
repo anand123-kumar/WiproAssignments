69e1dc767302daa1ff735e05d839c6163532490b
anand123-kumar <anandtupakula1234@gmail.com>
2025-08-01 10:50:24 +0530

    Update JUNITAssertionsDemo.java

--- src/main/java/com/junitprograms/JUNITAssertionsDemo.java
+++ src/main/java/com/junitprograms/JUNITAssertionsDemo.java
@@ -26,24 +26,22 @@ public class JUNITAssertionsDemo {
 		int var2=2;
 		int[] arithmetic1= {1,2,3,4,5};
 		int[] arithmetic2= {1,2,3,4,5};
-		Assert.assertEquals(obj3,obj4);//The assertEquals() method compares two objects for equality,//using their equals() method. 
+		Assert.assertEquals(obj3,obj4);. 
 		assertEquals(obj1,obj2);
-		//Check if two object references point to the same object TYPE(stack/heap)
-		//assertSame(obj3,obj4);//failure  bcos one is heap and another one is pool!
+		
 		assertSame(obj1,obj2);
-		//Check if two object references not point to the same object type
+		
 		assertNotSame(obj3,obj4);//different memory returns true
-		//assertNotSame(obj1,obj2);//same memory returns false
-		//Check that an object isn't null
+		
 		assertNotNull(obj1);
-		//Check that an object is null
+		
 		assertNull(obj5);
-		//Check that a condition is true
+		
 		assertTrue(var1 < var2);//1<2
-		//Check that a condition is false
+		
 		assertFalse(var1 > var2);//1>2
-		// Check whether two arrays are 
-		//equal to each other.
+		
+		
 		assertArrayEquals(arithmetic1,arithmetic2);
 
 

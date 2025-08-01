365d6b347cd5ca26902116a64023a93f78e12ec2
anand123-kumar <anandtupakula1234@gmail.com>
2025-08-01 11:10:06 +0530

    Update LoginServlet.java

--- src/main/java/myservletprograms/LoginServlet.java
+++ src/main/java/myservletprograms/LoginServlet.java
@@ -14,13 +14,13 @@ public class LoginServlet extends HttpServlet {
      String accNo = request.getParameter("accountNumber");
      String password = request.getParameter("password");
 
-     // Dummy validation
+   
      if(accNo.equals("12345") && password.equals("pass123")) {
          HttpSession session = request.getSession();
          session.setAttribute("name", "Anand Kumar");
          session.setAttribute("balance", 15000.0);
 
-         // Redirect to balance page
+        
          response.sendRedirect("BalanceServlet");
      } else {
          out.println("<h3>Invalid credentials! <a href='Login.html'>Try Again</a></h3>");

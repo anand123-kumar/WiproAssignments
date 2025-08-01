ae061db6b06eecd6cf98b8955308a42f75837e90
anand123-kumar <anandtupakula1234@gmail.com>
2025-08-01 11:07:52 +0530

    Update InputServlet.java

--- src/main/java/myservletprograms/InputServlet.java
+++ src/main/java/myservletprograms/InputServlet.java
@@ -7,15 +7,15 @@ import javax.servlet.http.*;
 @WebServlet("/input")
 public class InputServlet extends HttpServlet {
  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-     // Read form data
+   
      String name = request.getParameter("name");
      String marksStr = request.getParameter("marks");
 
-     // Set attributes to forward
+    
      request.setAttribute("name", name);
      request.setAttribute("marks", marksStr);
 
-     // Forward to GradeServlet
+   
      RequestDispatcher rd = request.getRequestDispatcher("grade");
      rd.forward(request, response);
  }

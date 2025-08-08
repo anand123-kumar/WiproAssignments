56a26c94f179271e15ac6333d7f5896f631702c4
anand123-kumar <anandtupakula1234@gmail.com>
2025-08-08 13:22:45 +0530

    initial

---
+++ src/test/java/wipeur/WiproSpringBootEurekaServer/WiproSpringBootEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package wipeur.WiproSpringBootEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiproSpringBootEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
